1244cce0e7e6067fc0497a70f5e3a368c800dec1
comdo-santhosh <sanss2620818@gmail.com>
2024-03-05 17:14:02 +0530

    variable c is updated

--- helloworld.java
+++ helloworld.java
@@ -6,6 +6,6 @@ class helloworld{
     public static void main(String[] args) {
         System.out.println("santhosh");
         System.out.println("My brother name is surenthar s");
-        int c = 3;
+        int c = 32;
     }
 }
