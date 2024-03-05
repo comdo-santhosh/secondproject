d7322acb154439f37ca06822457263675c5873f4
SANTHOSHKUMAR S <114432961+comdo-santhosh@users.noreply.github.com>
2024-03-05 16:56:19 +0530

    feat : Update helloworld.java added variable 'c'

--- helloworld.java
+++ helloworld.java
@@ -6,5 +6,6 @@ class helloworld{
     public static void main(String[] args) {
         System.out.println("Santhoshkumar s");
         System.out.println("My brother name is surenthar s");
+        int c = 3;
     }
 }
